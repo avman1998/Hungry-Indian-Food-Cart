184122e1fa6bc7d08deabe053c4d04b439605d4d
Aman Verma <84556693+avman1998@users.noreply.github.com>
2023-08-03 11:22:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const IMG_CDN_URL =
-"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 export const restaurantList = [
   {
     type: "restaurant",
